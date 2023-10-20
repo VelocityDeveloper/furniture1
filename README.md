96700e5dee9da032deca90135ba37637ac90a0bb
Velocity Developer <76415135+VelocityDeveloper@users.noreply.github.com>
2023-10-20 09:15:41 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# furniture1
+Velocity Child Theme Paket F Tour 1
+=================
+
+Child Theme for the Velocity System WordPress theme.
+
+https://www.tour1.velocitydeveloper.com/
---
+++ inc/part-footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer container text-center p-2" id="colophon">
+    <div class="site-info shadow text-white">
+        <small>
+            Copyright © <?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?>. All Rights Reserved.
+        </small>
+        <br>
+        <small class="opacity-50" style="font-size: .7rem;">
+            Design by <a class="text-white" href="https://velocitydeveloper.com" target="_blank" rel="noopener noreferrer"> Velocity Developer </a>
+        </small>
+    </div>
+    <!-- .site-info -->
+</footer>
